463043281f3c7cc83bf2ab883f210b6421b9deb8
NaldoRonz <ronaldomreid19@gmail.com>
2020-03-22 14:48:56 -0500

    Project 1

--- app/models.py
+++ app/models.py
@@ -12,18 +12,18 @@ class my_users(my_db.Model):
 	email = my_db.Column(my_db.String(35),nullable=False, unique=True)
 	location = my_db.Column(my_db.String(200),nullable=False)
 	biography = my_db.Column(my_db.String(1000),nullable=False)
-	photo = my_db.Column(my_db.String(20),nullable=False)
+	filename = my_db.Column(my_db.String(20),nullable=False)
 
-	def __init__(self,firstname,lastname,gender,email,location,biography,photo):
+	def __init__(self,firstname,lastname,gender,email,location,biography,filename):
 		self.firstname = firstname
 		self.lastname = lastname
 		self.gender = gender
 		self.email = email
 		self.location = location
 		self.biography = biography
-		self.photo = photo
+		self.filename = filename
 
 	def __repr__(self):
-		return f"my_users('{self.firstname}','{self.lastname}','{self.email}','{self.photo}')"
+		return f"my_users('{self.firstname}','{self.lastname}','{self.email}','{self.filename}')"
 
 my_db.create_all()
