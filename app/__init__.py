f3b7b2b784aaea767c5643bfc59561b6234bfe9e
NaldoRonz <ronaldomreid19@gmail.com>
2020-03-30 13:40:29 -0500

    Project 1

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ app.config["SECRET_KEY"] = "9_!Nald&$K8Y{i5*_H3r3}4618542184$@qlShu)_Un3<2gGu57"
 
 # Runs on port 5432 only works with default server postgres
 #app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:RSK4LFEg@localhost/postgres"
-app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql:////ujmngpfpsldxvj:322ecfd88990c001c4427c4cae110aa83097d3bdd6182e2786db75524e6e63b3@ec2-54-147-209-121.compute-1.amazonaws.com:5432/de54p28jgcb3kp'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://ujmngpfpsldxvj:322ecfd88990c001c4427c4cae110aa83097d3bdd6182e2786db75524e6e63b3@ec2-54-147-209-121.compute-1.amazonaws.com:5432/de54p28jgcb3kp'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 #For File Upload
